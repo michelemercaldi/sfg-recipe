6855f153e1ac094a0701da25f13509cd8e6fdd5f
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-01-14 10:10:10 +0100

    init repo

---
+++ src/test/java/mm/springframework/sfgrecipe/SfgrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package mm.springframework.sfgrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
