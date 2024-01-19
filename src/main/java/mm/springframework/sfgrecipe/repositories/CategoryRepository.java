a03f6a9ea400f800c019cf0a4c7101d4fc601bca
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-01-19 10:54:49 +0100

    140. Spring Data JPA Query Methods

--- src/main/java/mm/springframework/sfgrecipe/repositories/CategoryRepository.java
+++ src/main/java/mm/springframework/sfgrecipe/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package mm.springframework.sfgrecipe.repositories;
 import mm.springframework.sfgrecipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
