34e2587c31527d8d5dcc922b1acbe76fa3b67c06
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-01-19 13:30:54 +0100

    142. Assignment Review: Display List of Recipes

---
+++ src/main/java/mm/springframework/sfgrecipe/services/RecipeService.java
@@ -0,0 +1,13 @@
+package mm.springframework.sfgrecipe.services;
+
+import mm.springframework.sfgrecipe.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
---
+++ src/main/java/mm/springframework/sfgrecipe/services/RecipeServiceImpl.java
@@ -0,0 +1,28 @@
+package mm.springframework.sfgrecipe.services;
+
+import mm.springframework.sfgrecipe.domain.Recipe;
+import mm.springframework.sfgrecipe.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+    }
+}
