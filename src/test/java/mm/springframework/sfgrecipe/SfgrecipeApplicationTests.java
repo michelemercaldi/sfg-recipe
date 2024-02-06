28604345210b7967e997d73a5928a7df117b6d05
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-02-07 00:45:19 +0100

    192. Using WebJars with Spring Boot

--- src/test/java/mm/springframework/sfgrecipe/SfgrecipeApplicationTests.java
+++ src/test/java/mm/springframework/sfgrecipe/SfgrecipeApplicationTests.java
@@ -3,11 +3,12 @@ package mm.springframework.sfgrecipe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SfgrecipeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
