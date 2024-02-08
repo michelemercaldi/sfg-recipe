43528aa3c3f748008f1669dfa83adb4c08b7fb56
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-02-08 23:28:04 +0100

    197. Creating a Command Object and Type Conversions in Spring

---
+++ src/main/java/mm/springframework/sfgrecipe/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -0,0 +1,26 @@
+package mm.springframework.sfgrecipe.converters;
+
+import mm.springframework.sfgrecipe.commands.UnitOfMeasureCommand;
+import mm.springframework.sfgrecipe.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure, UnitOfMeasureCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure unitOfMeasure) {
+
+        if (unitOfMeasure != null) {
+            final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+            uomc.setId(unitOfMeasure.getId());
+            uomc.setDescription(unitOfMeasure.getDescription());
+            return uomc;
+        }
+        return null;
+    }
+}
