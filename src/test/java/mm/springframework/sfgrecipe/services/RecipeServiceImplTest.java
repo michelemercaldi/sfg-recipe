fe5953872f2d201c848cebed808b33bbbc2b2916
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-02-11 18:43:08 +0100

    199. Update a Recipe 200. Delete a Recipe

--- src/test/java/mm/springframework/sfgrecipe/services/RecipeServiceImplTest.java
+++ src/test/java/mm/springframework/sfgrecipe/services/RecipeServiceImplTest.java
@@ -1,6 +1,7 @@
 package mm.springframework.sfgrecipe.services;
 
 
+import mm.springframework.sfgrecipe.commands.RecipeCommand;
 import mm.springframework.sfgrecipe.converters.RecipeCommandToRecipe;
 import mm.springframework.sfgrecipe.converters.RecipeToRecipeCommand;
 import mm.springframework.sfgrecipe.domain.Recipe;
@@ -15,8 +16,8 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
+import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
 
@@ -54,13 +55,34 @@ public class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipeCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     public void getRecipesTest() throws Exception {
 
         Recipe recipe = new Recipe();
         HashSet receipesData = new HashSet();
         receipesData.add(recipe);
 
-        when(recipeService.getRecipes()).thenReturn(receipesData);
+        //when(recipeService.getRecipes()).thenReturn(receipesData);
+        when(recipeRepository.findAll()).thenReturn(receipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
@@ -69,4 +91,18 @@ public class RecipeServiceImplTest {
         verify(recipeRepository, never()).findById(anyLong());
     }
 
+    @Test
+    public void testDeleteById() throws Exception {
+
+        //given
+        Long idToDelete = Long.valueOf(2L);
+
+        //when
+        recipeService.deleteById(idToDelete);
+
+        //no 'when', since method has void return type
+
+        //then
+        verify(recipeRepository, times(1)).deleteById(anyLong());
+    }
 }
